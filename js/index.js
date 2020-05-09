9dee9cc59fc37824e873dbae1f65a68bd349d642
Ziegfred <ziegfredzorrilla23@gmail.com>
2020-05-09 14:39:46 +0800

    added search feature

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ var totalCases = document.querySelector("#totalCases");
 var newCases = document.querySelector("#newCases");
 var recoveredCases = document.querySelector("#recoveredCases");
 var totalDeaths = document.querySelector("#totalDeaths");
+var searchButton = document.querySelector("#searchButton");
+var searchInput = document.querySelector("#searchInput");
+var toggleButton = document.querySelector("#toggleButton");
 var jsonFile;
 var globalJSON;
 var opt;
@@ -32,6 +35,24 @@ function addCountryToSelect(country){
 		addOptionToSelect(country.country);
 	}
 }
+
+function toggleButtonEvent(event){
+	alert(event.textContent);
+	if(event.textContent == "Search Country"){
+		event.textContent = "Select Country";
+		searchButton.style.display = "block";
+		searchInput.style.display = "block";
+		countrySelect.style.display = "none";
+
+	}
+	else{
+		searchInput.style.display = "none";
+		searchButton.style.display = "none";
+		countrySelect.style.display = "block";
+		event.textContent = "Search Country";
+
+	}
+}
 function initSelect(){
 	jsonFile.forEach(addCountryToSelect);
 }
@@ -43,10 +64,17 @@ function findCountryIndex(){
 	return jsonFile.findIndex(isCountryExist);
 
 }
+function isCountryExistInput(country){
+	return country.country === searchInput.value;
+}
+function findCountryIndexInput(){
+	return jsonFile.findIndex(isCountryExistInput);
+}
 function specificCountryURLFormat(countryName){
 	var format = "https://corona.lmao.ninja/v2/countries/"+countryName+"?yesterday=true&strict=true&query";
 	return format;
 }
+
 function loadSpecificCountry(event){
 		
 	if(selectTag.value !== "Global"){
@@ -61,6 +89,24 @@ function loadSpecificCountry(event){
 		loadGlobalData();
 	}
 	
+}
+function loadSearch(event){
+
+	var countryIndex = findCountryIndexInput();
+
+	if(countryIndex === -1 && event.value === "Global"){
+		loadGlobalData();	
+	}
+	else if(countryIndex !== -1 && event.value !== "Global"){
+		countryName.value = jsonFile[countryIndex].country;
+		totalCases.value = jsonFile[countryIndex].cases;
+		newCases.value = jsonFile[countryIndex].todayCases;
+		recoveredCases.value = jsonFile[countryIndex].recovered;
+		totalDeaths.value = jsonFile[countryIndex].deaths;
+	}
+	else{
+		alert("Country not found or Country does not have COVID cases.");
+	}
 }
 function loadGlobalJSON(){
 	fetch("https://corona.lmao.ninja/v2/all?yesterday")
@@ -83,8 +129,26 @@ function loadGlobalData(){
 	totalDeaths.value = globalJSON.deaths;
 }
 
+toggleButton.addEventListener("click", function(event){
+	
+	if(this.textContent === "Search Country"){
+		this.textContent = "Select Country";
+		searchButton.style.display = "block";
+		searchInput.style.display = "block";
+		countrySelect.style.display = "none";
 
-selectTag.addEventListener("change", loadSpecificCountry)
+	}
+	else if(this.textContent === "Select Country"){
+		searchInput.style.display = "none";
+		searchButton.style.display = "none";
+		countrySelect.style.display = "block";
+		this.textContent = "Search Country";
+
+	}
+});
+
+searchButton.addEventListener("click", loadSearch)
+selectTag.addEventListener("change", loadSpecificCountry);
 getJSON();
 loadGlobalJSON();
 
